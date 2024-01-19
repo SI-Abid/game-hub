347fdb9991fde10236c23075f7d76c536ef3a56f
SI-Abid <siaabid5323@gmail.com>
2024-01-19 15:04:18 +0600

    reusable api client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,27 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "f158df987cfa4170b84432ec3ac58b36",
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
