4fb4c640ffa719c8970a8171765bc399031f6ca1
Saiham Islam Abid <saiham@inverseai.com>
2024-01-22 12:00:44 +0600

    handle errors

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 const GameDetailPage = () => {
-  return (
-    <div>GameDetailPage</div>
-  )
-}
+  return <div>GameDetailPage</div>;
+};
 
-export default GameDetailPage
+export default GameDetailPage;
