3f100e77382d4a5b6de9d11ffeee66910f19a624
Saiham Islam Abid <saiham@inverseai.com>
2024-01-22 15:48:44 +0600

    part-2 deploy ready

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "f158df987cfa4170b84432ec3ac58b36",
+    key: process.env.RAWG_API_KEY,
   },
 });
 
